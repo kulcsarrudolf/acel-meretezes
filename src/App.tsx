f813b4b10e4ce1363af84a53d812a4d4940eff10
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2021-09-16 22:10:32 +0300

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,33 @@ import { useState, useEffect } from 'react';
 
 import TextField from '@material-ui/core/TextField';
 
-import { values } from './service/values';
+// import { values } from './service/values';
 import { getValuesForGivenLambda } from './service/service';
 
 function App() {
-  const [allValues, setAllValues] = useState(values);
-  const [currentValues, setCurrentValues] = useState(getValuesForGivenLambda(0.2));
-  const [lambda, setLambda] = useState(0.2);
+  // TODO: table component
+  // const [allValues, setAllValues] = useState(values);
+  const [currentValues, setCurrentValues] = useState<any>(getValuesForGivenLambda(1.8));
+  const [lambda, setLambda] = useState('1.8');
 
   const handleChangeLambda = (e: any) => {
     setLambda(e.target.value);
-
-    if (isNaN(parseInt(lambda.toString(), 10)) && (lambda >= 0.2 || lambda <= 3)) {
-      console.log('test');
-      setCurrentValues(getValuesForGivenLambda(lambda));
-    }
   };
 
-  // useEffect(() => {
-  //   if (isNaN(parseInt(lambda.toString(), 10)) && (lambda >= 0.2 || lambda <= 3)) {
-  //     setCurrentValues(getValuesForGivenLambda(lambda));
-  //   }
-  // }, [lambda]);
+  useEffect(() => {
+    try {
+      setCurrentValues(getValuesForGivenLambda(parseFloat(lambda)));
+    } catch (error) {
+      // do nothing
+    }
+  }, [lambda]);
 
   return (
     <>
       <h1>Acél méretezés</h1>
 
       <TextField
+        style={{ maxWidth: '10rem', margin: '1rem' }}
         required
         id="filled-required"
         label="Required"
@@ -38,15 +37,17 @@ function App() {
         onChange={handleChangeLambda}
       />
 
-      <TextField
-        id="standard-read-only-input"
-        label="a0"
-        value={currentValues?.a0}
-        InputProps={{
-          readOnly: true,
-        }}
-        variant="outlined"
-      />
+      {['a0', 'a', 'b', 'c', 'd'].map((valueType) => (
+        <TextField
+          style={{ maxWidth: '10rem', margin: '1rem' }}
+          variant="outlined"
+          label={valueType}
+          value={currentValues[valueType]}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+      ))}
     </>
   );
 }
